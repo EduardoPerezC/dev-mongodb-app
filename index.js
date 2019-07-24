089cf1df9851970f83a03e324f829a35a6c980f0
EduardoPerezC <jpdevelopernet2015@icloud.com>
2019-07-24 17:02:21 -0500

    aaa

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require(`mongoose`)
 
 mongoose.connect(`mongodb://localhost/playground`)
-	.then(()=> console.log(‘connected’))
-	.catch((error)=>console.error(‘could not connect to mongodb’,error))
+	.then(()=> console.log(`connected`))
+	.catch((error)=>console.error(`could not connect to mongodb`,error))
