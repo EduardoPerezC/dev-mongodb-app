4f5f2364d1625330e7d34f72ed15e9ea3e20d3d6
EduardoPerezC <jpdevelopernet2015@icloud.com>
2019-08-13 12:39:10 -0500

    bbb

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function connect(){
 	.catch((error)=>console.error(`could not connect to mongodb`,error))	
 }
 
-app.use(`test`,testRouter)
+app.use('Test',testRouter)
 
 app.listen(port,()=>{
 	console.log(`listening on port ` + port)
